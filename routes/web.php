bde76ad9d7ae1f24054b9225de3594ef9715ffcd
Ahmad Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2020-12-31 23:45:07 +0700

    Create all questions view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+use App\Http\Controllers\QuestionsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('questions', QuestionsController::class);
