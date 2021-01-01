2da839726be999cfcb6924125bb89d27919890ea
Ahmad Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2021-01-01 11:45:46 +0700

    Styling votes, answers, and views

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -33,4 +33,15 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            } else
+                return "answered";
+        } else
+            return "unanswered";
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -6,11 +6,25 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Questions') }}</div>
-
                 <div class="card-body">
                     @foreach ($questions as $question)
-                    <div class="media">
-                        <div class="media-body">
+                    <div class="d-flex media">
+                        <div class="flex-4 counters">
+                            <div class="d-flex flex-column">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> 
+                                    {{ Str::plural('vote', $question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong>
+                                    {{ Str::plural('answer', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . Str::plural('view', $question->views) }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex-fill media-body">
                             <h3 class="mt-0">
                                 <a href="{{ $question->url }}">{{  $question->title }}</a>
                             </h3>
