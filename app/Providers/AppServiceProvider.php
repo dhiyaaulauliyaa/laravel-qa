5f3f351ad2bcfd351eebd4d7c777c32dc0ff71a0
Ahmad Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2021-01-02 01:02:57 +0700

    Create gates instance

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('update-question', function ($user, $question) {
+            return $user->id === $question->user_id;
+        });
+        
+        Gate::define('delete-question', function ($user, $question) {
+            return $user->id === $question->user_id;
+        });
     }
 }
