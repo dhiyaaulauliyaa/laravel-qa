a6eec173beb60cb15ffffc31d638f79ffc75554f
Ahmad Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2021-01-01 14:15:27 +0700

    Integrate request to controller

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AskQuestionRequest;
 use App\Models\Question;
 use Illuminate\Http\Request;
 
@@ -34,9 +35,9 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        dd('Store');
     }
 
     /**
