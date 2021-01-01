23ee5b7c40fe174d5f7bf31d7c49bcd997bd7d87
Ahmad Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2021-01-01 14:57:06 +0700

    Create flash message

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -14,6 +14,7 @@
                 </div>
 
                 <div class="card-body">
+                    @include('layouts._messages')
                     @foreach ($questions as $question)
                     <div class="d-flex media">
 
